0d9ff18744cba2b2c7e9981af0e5f80d6091aa86
Tran Bao Huy <huy23121994@gmail.com>
2018-12-01 08:32:17 +0700

    landing page

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import bannerImg from 'assets/img/banner.png';
+import aboutUsImg from 'assets/img/about_us.png';
+import supplies1Img from 'assets/img/supplies_1.png';
+import supplies2Img from 'assets/img/supplies_2.png';
+import footerImg from 'assets/img/footer.png';
+
 import 'normalize.css';
 import 'styles/index.scss';
 
 const App = () => (
   <div className='App'>
     <Navigation/>
-    <div>
-      <h1>It Works!</h1>
-      <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-      <p>Enjoy!</p>
-    </div>
+    <section>
+      <img src="img/banner.png" alt="" />
+    </section>
+    <section>
+      <img src={bannerImg} alt="" />
+      <img src={aboutUsImg} alt="" />
+      <img src={supplies1Img} alt="" />
+      <img src={supplies2Img} alt="" />
+      <img src={footerImg} alt="" />
+    </section>
   </div>
 );
 
