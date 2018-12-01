a1a90d86eae0082f195117fdfad0bce7d6aa8953
Tuan Nguyen <edwardevans094@gmail.com>
2018-12-01 12:59:51 +0700

    add detail confir,

--- src/components/DetailConfirm.jsx
+++ src/components/DetailConfirm.jsx
@@ -1,45 +1,144 @@
 import React, { Component } from 'react';
 import env from "../env"
 import * as utils from "../services/utils"
+import Modal from 'react-modal';
+import breadcrumb from '../assets/img/breadcrumb.png';
+import title_project from '../assets/img/title_project.png';
+import content_project from '../assets/img/content_project.png';
+import right_sidebar from '../assets/img/right_sidebar.png';
+import '../assets/css/detail.scss';
 
+const tokensSupport = Object.keys(env.tokens).map(t => ({
+  symbol: t,
+  name: env.tokens[t].name,
+  address: env.tokens[t].address
+}))
 
-class DetailConfirm extends Component {
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#app')
+
+class Detail extends Component {
   constructor() {
     super()
 
     this.campaignAddr = utils.getParameterByName('campaignAddr')
     console.log("______+++++++++++++++++", this.campaignAddr)
     this.state = {
+      tokenAddr: "",
+      amount: 0,
+      deligatorAddr: "",
+      modalIsOpen: false
     }
   }
 
-
-  onConfirm = () => {
-    console.log("___________confirm")
+  onChangeToken = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      tokenAddr: e.target.value
+    })
   }
 
-  onReject = () => {
-    console.log("___________ reject")
+  onChangeAmount = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      amount: e.target.value
+    })
+  }
+
+  onChangeDeligator = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      deligatorAddr: e.target.value
+    })
+  }
+
+  openModal = () =>  {
+    this.setState({modalIsOpen: true});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    console.log("************** submit form: ", this.state.tokenAddr, this.state.amount, this.state.deligatorAddr)
+  }
+
+  onClose = (e) => {
+    this.setState({modalIsOpen: false});
   }
 
   render() {
     return (
       <div>
-        This view show detail of campain
+        <div className="page-content">
+          <img src={breadcrumb} alt=""/>
+          <img className="right-sidebar" src={right_sidebar} alt=""/>
+          <div className="article-content">
+            <img src={title_project} alt=""/>
+            <div className="form-approval">
+              <div className="title">Request withdraw</div>
+              <div className="d-inline-block mr-5">Amount: 12 ETH</div>
+              <div className="d-inline-block">Due Date: 22 Jan 2019</div>
+              <div className="mt-1">Due Date: 22 Jan 2019</div>
+              <div className="group-btn">
+                <button className="h-btn mr-3">Reject</button>
+                <button className="h-btn h-blue">Accept</button>
+              </div>
+            </div>
+            <div className="d-flex justify-content-between balance-area">
+              <div className="title">Current Balance: </div>
+              <div>
+                <div className="balance">16 ETH</div>
+                <button className="h-btn">History</button>
+              </div>
+            </div>
+            <img src={content_project} alt=""/>
+          </div>
+        </div>
+        <br />
+        
+      <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Select Token</label>
+            <select onChange={this.onChangeToken.bind(this)}>
+              {tokensSupport.map((t, i) => (
+                <option value={t.address} key={i}>{t.name}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Amount</label>
+            <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Amount" onChange={this.onChangeAmount} />
+          </div>
+          <div className="form-group form-check">
+            <label className="form-check-label" htmlFor="exampleCheck1">Select delegator</label>
+            <div className="input-group mb-3">
+              <input type="checkbox" name="vehicle1" value="Bike" checked={this.state.deligatorAddr == "Bike"} onChange={this.onChangeDeligator}/> I have a bike<br />
+              <input type="checkbox" name="vehicle2" value="Car" checked={this.state.deligatorAddr == "Car"} onChange={this.onChangeDeligator}/> I have a car<br />
+              <input type="checkbox" name="vehicle3" value="Boat" checked={this.state.deligatorAddr == "Boat"} onChange={this.onChangeDeligator}/> I have a boat<br />
+            </div>
+          </div>
+          <button className="btn btn-primary" onClick={this.onSubmit}>Submit</button>
+          <button className="btn" onClick={this.onClose}>close</button>
+        </form>
 
-    - normal user can donate for campain
-      input: amount, Address of user want to deligate
-
-      if( want revert money) show button revert money for user
-
-    - creator of campain
-      show button clain money, input amount
-      wait for DAO approve
-
-    <br />
-    <button onClick={this.onConfirm}>Confirm</button>
-    <button onClick={this.onReject}>Reject</button>
-    
+        </Modal>
       </div>
     )
   }
@@ -47,4 +146,4 @@ class DetailConfirm extends Component {
 }
 
 
-export default DetailConfirm;
+export default Detail;
--- src/routes.jsx
+++ src/routes.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 import List from './components/List';
 import Detail from './components/Detail';
-// import Detail from './components/Detail';
+import DetailConfirm from './components/DetailConfirm';
 import Approve from './components/Approve';
 
 import './assets/css/app.scss';
@@ -22,7 +22,7 @@ const Routes = () => (
         <Route exact path="/" component={Home}/>
         <Route path="/list" component={List}/>
         <Route path="/detail" component={Detail}/>
-        <Route path="/detail-confirm" component={Detail}/>
+        <Route path="/detail-confirm" component={DetailConfirm}/>
         <Route path="/deligate" component={Approve}/>
       </div>
       <Footer />
